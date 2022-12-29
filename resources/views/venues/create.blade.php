f153355b7fc20f1b86184058eac0738c5eb3f5b8
M Firkins <73889650+mfirkins@users.noreply.github.com>
2022-12-29 22:09:07 +0000

    Add enctype to POST form, change some names for request

--- resources/views/venues/create.blade.php
+++ resources/views/venues/create.blade.php
@@ -3,46 +3,42 @@
 @section('page-name', 'Create a Venue')
 @section('content')
 
-    <x-form method="POST" action="/venues/store">
+    <x-form method="POST" action="/venues" enctype="multipart/form-data">
         <div class="row form-group">
             <div class="column">
                 <br>
-                <label for="eventName">Venue Name</label>
-                <input type="text" class="form-control" id="eventName" placeholder="My Amazing Event">
+                <label for="venueName">Venue Name</label>
+                <input type="text" class="form-control" id="venueName" value="{{ old('name') }}" placeholder="My Cool Venue" name="name">
                 <br>
                 <label for="description">Description</label>
-                <textarea class="form-control" id="description" rows="6"></textarea>
+                <textarea class="form-control" id="description" value="{{ old('description') }}" rows="6" name="description"></textarea>
 
             </div>
             <div class="column">
                 <br>
-                <label for="category">Accessible</label>
-                <select class="form-control" id="category">
-                    <option>
-                        Available
-                    </option>
-                    <option>
-                        Unavailable
-                    </option>
+                <label for="accessible">Accessible</label>
+                <select class="form-control" id="accessible" value="{{ old('accessible') }}" name="accessible">
+                    <option>Available</option>
+                    <option>Unavailable</option>
                 </select>
                 <br>
                 <label for="image">Image</label>
-                <input id="image" type="file" class="form-control" name="image">
+                <input id="image" type="file" class="form-control" value="{{ old('image') }}" name="image">
                 <br>
                 <label for="city">City</label>
-                <input type="text" class="form-control" id="city" placeholder="Some Cool City">
+                <input type="text" class="form-control" id="city" placeholder="Birmingham" value="{{ old('city') }}" name="city">
                 <br>
                 <div class="column">
-                    <label for="eventName">Longitude</label>
-                    <input type="text" class="form-control" id="city" placeholder="53.7654">
+                    <label for="long">Longitude</label>
+                    <input type="text" class="form-control" id="long" placeholder="53.7654" value="{{ old('longitude') }}" name="longitude">
                 </div>
                 <div class="column">
-                    <label for="eventName">Latitude</label>
-                    <input type="text" class="form-control" id="city" placeholder="-1.67839">
+                    <label for="lat">Latitude</label>
+                    <input type="text" class="form-control" id="lat" placeholder="-1.67839" value="{{ old('latitude') }}" name="latitude">
                 </div>
             </div>
         </div>
         <br>
-        <button class="btn btn-contrast text-white" type="button"> Create Venue </button>
+        <button class="btn btn-contrast text-white" type="submit"> Create Venue </button>
     </x-form>
 @endsection
