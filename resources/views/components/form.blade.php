b5a5b46d5d5ed31ad45d65d96b8de50edbb049ef
M Firkins <73889650+mfirkins@users.noreply.github.com>
2022-12-29 16:53:37 +0000

    corresponding blade file for form component

---
+++ resources/views/components/form.blade.php
@@ -0,0 +1,45 @@
+<style>
+    #div-container {
+        text-align: center;
+    }
+
+    #div-box {
+
+        background: linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7));
+        border: 2px solid #ffffff;
+        border-radius: 1rem;
+        display: inline-block;
+        padding-left: 50px;
+        padding-right: 50px;
+        padding-top: 20px;
+        padding-bottom: 20px;
+    }
+
+    .column {
+        float: left;
+        width: 50%;
+        justify-content: center;
+        align-items: center;
+    }
+
+
+
+    label {
+        color: white;
+    }
+
+    a {
+        color: white;
+    }
+
+    p {
+        color: white;
+    }
+</style>
+
+<form method={{ $method }} action={{ $action }} id="div-container">
+    @csrf
+    <div id="div-box">
+        {{ $slot }}
+    </div>
+</form>
