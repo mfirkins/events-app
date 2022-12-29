488eda4991416e1b290ab76bc76e500ef7a3f03d
M Firkins <73889650+mfirkins@users.noreply.github.com>
2022-12-29 22:09:58 +0000

    add error handling on base layout with a venue navlink

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -86,6 +86,9 @@
                             </ul>
                         </li>
                         <li class="nav-item">
+                            <a class="nav-link text-white" href="{{ route('venues.index') }}">Venues</a>
+                        </li>
+                        <li class="nav-item">
                             <a class="btn btn-contrast text-card-bg" href="{{ route('create_event') }}">+ Create an
                                 Event</a>
                         </li>
@@ -138,6 +141,19 @@
             <div id="content-div">
                 @hasSection('page-name')
                     <h1 class="text-center text-white">@yield('page-name')</h1>
+                @endif
+
+                @if ($errors->any())
+                    @foreach ($errors->all() as $error)
+                        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16">
+                                <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                              </svg>
+                            <b>Houston. We have a problem:</b> {{ $error }}
+                            <button type="button" class="btn-close" data-bs-dismiss="alert"
+                                aria-label="Close"></button>
+                        </div>
+                    @endforeach
                 @endif
                 @yield('content')
             </div>
