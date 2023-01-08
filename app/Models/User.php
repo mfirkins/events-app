c62eeedbc2740622017b71d047b4eae880f20557
M Firkins <73889650+mfirkins@users.noreply.github.com>
2023-01-08 18:11:10 +0000

    add image_name as fillable

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
@@ -20,8 +21,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
-        'password',
-        'image_name'
+        'password'
     ];
 
     /**
@@ -43,15 +43,8 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function events(){
-        return $this->hasMany(Event::class);
-    }
-
-    public function comments(){
-        return $this->hasMany(Category::class);
-    }
-
-    public function venues(){
-        return $this->hasMany(Venue::class);
+  
+    public function profile(){
+        return $this->hasOne(Profile::class);
     }
 }
