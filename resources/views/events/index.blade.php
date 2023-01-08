366f841436f0d7a1c675b5e7e3aec59b48c045ea
M Firkins <73889650+mfirkins@users.noreply.github.com>
2023-01-08 18:23:52 +0000

    add index page for events

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -15,7 +15,7 @@
 @section('content')
     <div class="container-fluid mb-4">
         <div class="card-group">
-            @foreach ($events->sortBy('time') as $event)
+            @foreach ($events as $event)
                 <div class="col mt-4">
                     <div class="shadow-lg p-1 card bg-light mx-auto" style="width: 18rem;">
                         <img src="/storage/images/event_pictures/{{ $event->image_name }}" class="card-img-top" alt="event description">
@@ -23,6 +23,7 @@
                             <h5 class="card-title"><b>{{ $event->name }}</b></h5>
                             <h6>Date: <b>{{ Carbon\Carbon::parse($event->time)->toFormattedDateString() }}</b></h6>
                             <h6>Time: <b>{{ Carbon\Carbon::parse($event->time)->format('g:ia') }}</b></h6>
+                            
                             <h6>City: <b>{{ $event->venue->city }}</b></h6>
                             <h6>Cost: <b>£{{ $event->cost }}</b></h6>
                             <a href="/{{ $event->id }}" class="btn btn-primary">Details</a>
@@ -31,5 +32,6 @@
                 </div>
             @endforeach
         </div>
+        {{ $events->links() }}
     </div>
 @endsection
