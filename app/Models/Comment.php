2691fe21e799bc20a854f475dc12975114362032
M Firkins <73889650+mfirkins@users.noreply.github.com>
2023-01-08 18:04:07 +0000

    add content as a fillable for comment model

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,16 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class Comment extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'content'
+    ];
+
     public function event(){
         return $this->belongsTo(Event::class);
     }
 
-    public function user(){
-        return $this->belongsTo(User::class);
+    public function profile(){
+        return $this->belongsTo(Profile::class);
     }
 }
