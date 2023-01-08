a397acd87dab25a1022318a9debb712c4cbb3757
M Firkins <73889650+mfirkins@users.noreply.github.com>
2023-01-08 18:01:09 +0000

    add auth role check and pagination to event controller

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -18,7 +18,7 @@ class EventController extends Controller
      */
     public function index()
     {
-        $events = Event::all();
+        $events = Event::orderBy('time')->paginate(10);
         return view('events.index', ['events' => $events]);
 
     }
@@ -30,12 +30,14 @@ class EventController extends Controller
      */
     public function create()
     {
-        $categories = Category::all();
-        $venues = Venue::all();
-        if (Auth::check()) {
+        if (Auth::check() and Auth::user()->hasRole(['Visitor', 'Verified Venue', 'Admin'])) {
+            $categories = Category::all();
+            $venues = Venue::all();
             return view('events.create', ['categories' => $categories, 'venues' => $venues]);
         } else {
-            return redirect('/login');
+            session()->flash('message', "You do not have permission to create events");
+
+            return redirect()->route('home');
         }
 
     }
@@ -48,50 +50,67 @@ class EventController extends Controller
      */
     public function store(Request $request)
     {
-        $validateRules = [
-            'name' => 'required|max:255',
-            'description' => 'required|max:1000',
-            'image' => 'mimes:jpg,png,jpeg|max:5048',
-            'date' => 'required|date|after:today',
-            'amount' => 'required|numeric|between: 1, 100000',
-            'cost' => 'required|numeric|decimal:2|between:0, 10000',
-        ];
-        $hostValidate = ['host' => 'required|max:255'];
+        if (Auth::check() and Auth::user()->hasRole(['Visitor', 'Verified Venue', 'Admin'])) {
+            $validateRules = [
+                'name' => 'required|max:255',
+                'description' => 'required|max:1000',
+                'image' => 'mimes:jpg,png,jpeg|max:5048',
+                'date' => 'required|date|after:today',
+                'selected-categories' => 'required',
+                'amount' => 'required|numeric|between: 1, 100000',
+                'cost' => 'required|numeric|decimal:2|between:0, 10000',
+            ];
+            $hostValidate = ['host' => 'required|max:255'];
 
-        if ($request['use_user'] == null) {
-            array_merge($validateRules, $hostValidate);
-            $validatedData = $request->validate($validateRules);
-            $host_name = $validatedData['host'];
+            if ($request['use_user'] == null) {
+                array_merge($validateRules, $hostValidate);
+                $validatedData = $request->validate($validateRules);
+                $host_name = $validatedData['host'];
+            } else {
+                $validatedData = $request->validate($validateRules);
+                $host_name = Auth::user()->name;
+            }
+
+            if ($request->image != null) {
+                $imageName = time() . '_' . $request->name . '.' . $request->image->extension();
+                $request->image->storeAs('images/event_pictures', $imageName, 'public');
+
+            } else {
+                $imageName = null;
+            }
+
+            $categories = $validatedData['selected-categories'];
+            $categories = explode(',', $categories);
+            $categories = array_filter($categories);
+
+
+            $datetime = $validatedData['date'] . ' ' . $request['time'];
+
+            $event = new Event;
+            $event->name = $validatedData['name'];
+            $event->description = $validatedData['description'];
+            $event->image_name = $imageName;
+            $event->time = Carbon::parse($datetime)->toDateTime();
+            $event->profile_id = Auth::user()->profile->id;
+            $event->venue_id = Venue::where('name', $request['venue'])->get()->first()->id;
+            $event->host = $host_name;
+            $event->cost = $validatedData['cost'];
+            $event->tickets = $validatedData['amount'];
+            $event->save();
+            foreach ($categories as $category) {
+                $event->categories()->attach(Category::where('name', $category)->get()->first()->id);
+            }
+
+
+
+            session()->flash('message', "Event, $event->name , was successfully created");
+
+            return redirect()->route('home');
         } else {
-            $validatedData = $request->validate($validateRules);
-            $host_name = Auth::user()->name;
+            session()->flash('message', "You do not have permission to create an event");
+
+            return redirect()->route('home');
         }
-        
-        if ($request->image != null) {
-            $imageName = time() . '_' . $request->name . '.' . $request->image->extension();
-            $request->image->storeAs('images/event_pictures', $imageName, 'public');
-
-        } else {
-            $imageName = null;
-        }
-
-        $datetime = $validatedData['date'] . ' ' . $request['time'];
-
-        $event = new Event;
-        $event->name = $validatedData['name'];
-        $event->description = $validatedData['description'];
-        $event->image_name = $imageName;
-        $event->time = Carbon::parse($datetime)->toDateTime();
-        $event->user_id = Auth::id();
-        $event->venue_id = Venue::where('name', $request['venue'])->get()->first()->id;
-        $event->host = $host_name;
-        $event->cost = $validatedData['cost'];
-        $event->tickets = $validatedData['amount'];
-        $event->save();
-
-        session()->flash('message', "Event, $event->name , was successfully created");
-
-        return redirect()->route('home');
     }
 
     /**
@@ -103,8 +122,9 @@ class EventController extends Controller
     public function show($id)
     {
         $event = Event::findOrFail($id);
+        $event_comments = $event->comments()->paginate(5);
 
-        return view('events.show', ['event' => $event]);
+        return view('events.show', ['event' => $event, 'comments' => $event_comments]);
     }
 
     /**
@@ -115,7 +135,14 @@ class EventController extends Controller
      */
     public function edit($id)
     {
-        //
+        $event = Event::findOrFail($id);
+        if (Auth::check() and (Auth::user() == $event->profile->user or Auth::user()->hasRole('Admin'))) {
+            return view('events.edit', ['event' => $event]);
+        } else {
+            session()->flash('message', "You do not have permission to edit an event");
+
+            return redirect()->route('home');
+        }
     }
 
     /**
@@ -127,7 +154,34 @@ class EventController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $event = Event::findOrFail($id);
+        if (Auth::check() and (Auth::user() == $event->profile->user or Auth::user()->hasRole('Admin'))) {
+
+            $validateRules = [
+                'name' => 'required|max:255',
+                'description' => 'required|max:2000',
+                'amount' => 'required|numeric|between: 1, 100000',
+                'cost' => 'required|numeric|decimal:2|between:0, 10000',
+            ];
+
+            $validatedData = $request->validate($validateRules);
+            
+            $event->update([
+                'name' => $validatedData['name'],
+                'description' => $validatedData['description'],
+                'cost' => $validatedData['cost'],
+                'tickets' => $validatedData['amount']
+            ]);
+
+            session()->flash('message', "Event, $event->name , was successfully updated");
+
+            return redirect()->route('home');
+        } else {
+            session()->flash('message', "You do not have permission to update an event");
+
+            return redirect()->route('home');
+        }
+
     }
 
     /**
@@ -138,6 +192,19 @@ class EventController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $event = Event::findOrFail($id);
+
+        if ((Auth::check() and (Auth::user() == $event->profile->user or Auth::user()->hasRole('Admin')))) {
+
+            $event->delete();
+
+            session()->flash('message', "Event, $event->name , was successfully deleted");
+
+            return redirect()->route('home');
+        } else {
+            session()->flash('message', "You do not have permission to delete this event");
+
+            return redirect()->route('home');
+        }
     }
 }
